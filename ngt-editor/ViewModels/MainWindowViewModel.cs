a1513711c55640462070b33710327cd76264194b
ZukTol <sldancer@yandex.ru>
2021-02-09 22:20:11 +0300

    add Avalonia.Material package

--- ngt-editor/ViewModels/MainWindowViewModel.cs
+++ ngt-editor/ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,9 @@ namespace ngt_editor.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Avalonia!";
+
+        public void OnOpen()
+        {
+        }
     }
 }
